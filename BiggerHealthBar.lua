5c71d92663667392f95496d0169b1d01d6ccd2c0
Slothpala <slothpala@gmail.com>
2023-07-15 19:32:01 +0200

    First Commit

---
+++ BiggerHealthBar.lua
@@ -0,0 +1,35 @@
+--[[
+	Created by Slothpala 
+--]]
+local PlayerFrameHealthBar = PlayerFrame.PlayerFrameContent.PlayerFrameContentMain.HealthBarArea.HealthBar
+local PlayerFrameManaBar = PlayerFrame.PlayerFrameContent.PlayerFrameContentMain.ManaBarArea.ManaBar
+-- inital setup to never show the manabar
+PlayerFrameManaBar:Hide(); PlayerFrameManaBar:HookScript("OnShow",PlayerFrameManaBar.Hide )
+PlayerFrameManaBar.ManaBarMask:Hide()
+
+local function removeMaskTexture()
+	for _,v in pairs ({
+		PlayerFrameHealthBar:GetStatusBarTexture(),
+		PlayerFrameHealthBar.MyHealPredictionBar,
+		PlayerFrameHealthBar.OtherHealPredictionBar,
+		PlayerFrameHealthBar.TotalAbsorbBar,
+		PlayerFrameHealthBar.TotalAbsorbBarOverlay,
+		PlayerFrameHealthBar.OverAbsorbGlow,
+		PlayerFrameHealthBar.OverHealAbsorbGlow,
+		PlayerFrameHealthBar.HealAbsorbBar,
+		PlayerFrameHealthBar.HealAbsorbBarLeftShadow,
+		PlayerFrameHealthBar.HealAbsorbBarRightShadow,
+		PlayerFrame.PlayerFrameContent.PlayerFrameContentMain.HealthBarArea.PlayerFrameHealthBarAnimatedLoss:GetStatusBarTexture()
+	}) do
+		if v:GetMaskTexture(1) ~= nil then
+			v:RemoveMaskTexture(v:GetMaskTexture(1))
+		end
+	end
+end
+removeMaskTexture() -- we have to remove it as the game froces the texture to stay in its original state otherwise
+-- imo the best function to hook as it get called very rarely but everytime when we need it i.e vehicle frame
+hooksecurefunc("PlayerFrame_ToPlayerArt", function(self)
+	PlayerFrameHealthBar:SetHeight(31) -- 31 = statusbar + manabar + seperator by trial and error
+end)
+
+
