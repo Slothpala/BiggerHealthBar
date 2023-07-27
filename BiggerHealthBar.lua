dce9d05f2f852ca7161d486f70b697feb40eeffe
Slothpala <slothpala@gmail.com>
2023-07-27 19:37:03 +0200

    removed HUD folder added 4 textures added mask texture new mthod of chaning the player frame texture so no more custom texture copying necessary

--- BiggerHealthBar.lua
+++ BiggerHealthBar.lua
@@ -1,40 +1,63 @@
 --[[
 	Created by Slothpala 
 --]]
+
 local PlayerFrameHealthBar = PlayerFrame.PlayerFrameContent.PlayerFrameContentMain.HealthBarArea.HealthBar
-local PlayerFrameManaBar = PlayerFrame.PlayerFrameContent.PlayerFrameContentMain.ManaBarArea.ManaBar
+local PowerBar = PlayerFrame.PlayerFrameContent.PlayerFrameContentMain.ManaBarArea.ManaBar
 -- inital setup to never show the manabar
 for _,ressourcebar in pairs({
-	PlayerFrameManaBar,
-	InsanityBarFrame
+	PowerBar,
+	InsanityBarFrame,
 }) do
 	ressourcebar:Hide()
 	ressourcebar:HookScript("OnShow",ressourcebar.Hide)
 end
 
-local function removeMaskTexture()
-	for _,v in pairs ({
-		PlayerFrameHealthBar:GetStatusBarTexture(),
-		PlayerFrameHealthBar.MyHealPredictionBar,
-		PlayerFrameHealthBar.OtherHealPredictionBar,
-		PlayerFrameHealthBar.TotalAbsorbBar,
-		PlayerFrameHealthBar.TotalAbsorbBarOverlay,
-		PlayerFrameHealthBar.OverAbsorbGlow,
-		PlayerFrameHealthBar.OverHealAbsorbGlow,
-		PlayerFrameHealthBar.HealAbsorbBar,
-		PlayerFrameHealthBar.HealAbsorbBarLeftShadow,
-		PlayerFrameHealthBar.HealAbsorbBarRightShadow,
-		PlayerFrame.PlayerFrameContent.PlayerFrameContentMain.HealthBarArea.PlayerFrameHealthBarAnimatedLoss:GetStatusBarTexture()
-	}) do
-		if v:GetMaskTexture(1) ~= nil then
-			v:RemoveMaskTexture(v:GetMaskTexture(1))
-		end
+--coords left,right,top,bottom
+local textures = {
+	FrameTexture = {
+		path = "Interface\\AddOns\\BiggerHealthBar\\Textures\\BiggerHealthBar_FrameTexture.tga",
+		coords = {26.5/256, 223.5/256, 27/128, 97/128}
+	},
+	FrameFlash = {
+		path = "Interface\\AddOns\\BiggerHealthBar\\Textures\\BiggerHealthBar_FrameFlash.tga",
+		coords = {27.5/256, 219/256, 26/128, 96/128}
+	},
+	AlternateFrameTexture = {
+		path = "Interface\\AddOns\\BiggerHealthBar\\Textures\\BiggerHealthBar_AlternateFrameTexture.tga",
+		coords = {26.5/256, 223.5/256, 27/128, 100/128}
+	},
+	AlternateFrameFlash = {
+		path = "Interface\\AddOns\\BiggerHealthBar\\Textures\\BiggerHealthBar_AlternateFrameFlash.tga",
+		coords = {27.5/256, 219/256, 26/128, 96.5/128}
+	},
+	Mask = {
+		path = "Interface\\AddOns\\BiggerHealthBar\\Textures\\BiggerHealthBar_PlayerFrameHealthMask.tga",
+		coords = {2/128, 126/128, 15/64, 52/64}
+	},
+} 
+
+hooksecurefunc("PlayerFrame_ToPlayerArt", function()
+	local frameContainer = PlayerFrame.PlayerFrameContainer
+	local isAlterntePowerFrame = PlayerFrame.activeAlternatePowerBar
+	local frameTexture = isAlterntePowerFrame and frameContainer.AlternatePowerFrameTexture or frameContainer.FrameTexture
+	if isAlterntePowerFrame then
+		frameTexture:SetTexture(textures["AlternateFrameTexture"].path)
+		frameTexture:SetTexCoord(unpack(textures["AlternateFrameTexture"].coords))
+		frameContainer.FrameFlash:SetTexture(textures["AlternateFrameFlash"].path)
+		frameContainer.FrameFlash:SetTexCoord(unpack(textures["AlternateFrameFlash"].coords))
+		PlayerFrameHealthBar.HealthBarMask:SetPoint("TOPLEFT",PlayerFrameHealthBar,-2,9)
+		PlayerFrameHealthBar.HealthBarMask:SetPoint("BOTTOMRIGHT",PlayerFrameHealthBar,2,-10)
+	else
+		frameTexture:SetTexture(textures["FrameTexture"].path)
+		frameTexture:SetTexCoord(unpack(textures["FrameTexture"].coords))
+		frameContainer.FrameFlash:SetTexture(textures["FrameFlash"].path)
+		frameContainer.FrameFlash:SetTexCoord(unpack(textures["FrameFlash"].coords))
+		PlayerFrameHealthBar.HealthBarMask:SetTexture(textures["Mask"].path)
+		PlayerFrameHealthBar.HealthBarMask:SetPoint("TOPLEFT",PlayerFrameHealthBar,-3,7)
+		PlayerFrameHealthBar.HealthBarMask:SetPoint("BOTTOMRIGHT",PlayerFrameHealthBar,2,-12)
 	end
-end
-removeMaskTexture() -- we have to remove it as the game froces the texture to stay in its original state otherwise
--- imo the best function to hook as it get called very rarely but everytime when we need it i.e vehicle frame
-hooksecurefunc("PlayerFrame_ToPlayerArt", function(self)
-	PlayerFrameHealthBar:SetHeight(31) -- 31 = statusbar + manabar + seperator by trial and error
+	PlayerFrameHealthBar:SetHeight(31)
 end)
 
 
